3dcb5b3f8fcaed2472fcfcccae18cb26ed92c4ef
brandondang1 <74919672+brandondang1@users.noreply.github.com>
2026-01-30 14:21:17 -0500

    Update prime_gap_mpi.c

--- prime_gap_mpi.c
+++ prime_gap_mpi.c
@@ -3,22 +3,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-/* How does this code work:
- * Biggest potential prime is n, the biggest possible factor of n is sqrt(n)
- * Calculate all the primes up to sqrt(n) and call them "primes"
- *
-*/
-#define SEGMENT_SIZE 1000000 // Just did 1 million but the theory is that if its small it fits within the CPU Cache
 
+/***************************************************************
+CP431 Assignment 1
+---------------
+Biggest potential prime is n, the biggest possible factor of n is sqrt(n)
+Calculate all the primes up to sqrt(n) and call them "primes"
+
+@author: Brandon Dang, Connor Doidge, Jackson Dow, Omar El-Baytam, Kerem Erkoc
+ ***************************************************************/
+
+#define SEGMENT_SIZE 1000000 // Defined 1 million but the theory is that if its small it fits within the CPU Cache
+
+/***************************************************************
+@method:
+simple_sieve
+---------------
+computes all prime numbers up to sqrt(n) using the Sieve of
+Eratosthenes
+
+@parameter:
+sqrt_n - square root of the upper bound n
+prime_count - output pointer storing number of primes found
+
+@return:
+pointer to array containing all primes <= sqrt(n)
+***************************************************************/
 int* simple_sieve(long sqrt_n, int *prime_count) { // Finds prime numbers up to sqrt(n) to use in the segmented sieve.
     char *is_prime = (char *)malloc(sqrt_n + 1); // Create a character array of primes up to sqrt(n) + 1
     memset(is_prime, 1, sqrt_n + 1); // Assume all characters are primes.
     is_prime[0] = is_prime[1] = 0; // 0 and 1 wont be used to calculate other primes.
 
     for (long i = 2; i * i <= sqrt_n; i++) { // A simple seive from 2 to sqrt(n)
+
         if (is_prime[i]) { // if a prime is found
+
             for (long j = i * i; j <= sqrt_n; j += i) is_prime[j] = 0; // mark all multiples to sqrt(n) as non primes.
+
         }
+
     }
 
     // calculate amount of primes to use in prime array definition
@@ -29,7 +52,9 @@ int* simple_sieve(long sqrt_n, int *prime_count) { // Finds prime numbers up to 
     int *primes = (int *)malloc(count * sizeof(int));
     int index = 0;
     for (long i = 2; i <= sqrt_n; i++) { // loop through all values from 2 to sqrt(n)
+
         if (is_prime[i]) primes[index++] = (int)i; // if its a prime add it to primes[index]
+
     }
 
     free(is_prime);
@@ -37,7 +62,25 @@ int* simple_sieve(long sqrt_n, int *prime_count) { // Finds prime numbers up to 
     return primes;
 }
 
-// --- FUNCTION: Sieve a specific segment and update gap statistics ---
+/***************************************************************
+@method:
+simple_sieve
+---------------
+Sieve a specific segment and update gap statistics
+
+@parameter:
+seg_low - lower bound 
+seg_high - upper bound
+primes - array of primes <= sqrt(n)
+prime_count - number of primes
+local_max - largest prime gap
+prep_p - previous prime encountered
+first_p - first prime found
+last_p - last prime found
+
+@return:
+None
+ ***************************************************************/
 void sieve_segment(long seg_low, long seg_high, int *primes, int prime_count,
                    long *local_max, long *prev_p, long *first_p, long *last_p) {
 
@@ -46,19 +89,24 @@ void sieve_segment(long seg_low, long seg_high, int *primes, int prime_count,
     memset(segment, 0, range); // 0 is prime
 
     for (int i = 0; i < prime_count; i++) {
+
         long p = primes[i];
         // Calculate the first multiple of p within this segment
         long start = (seg_low + p - 1) / p * p;
-        if (start < p * p) start = p * p; // Don't cross out the prime prime itself
 
+        if (start < p * p) start = p * p; // Don't cross out the prime prime itself
         for (long j = start; j <= seg_high; j += p) {
+
             segment[j - seg_low] = 1; // Mark as composite
         }
+
     }
 
     // Process the results of this segment
     for (int i = 0; i < range; i++) {
+
         if (!segment[i]) {
+
             long curr = seg_low + i;
             if (*first_p == -1) *first_p = curr;
             if (*prev_p != -1) {
@@ -67,9 +115,13 @@ void sieve_segment(long seg_low, long seg_high, int *primes, int prime_count,
             }
             *prev_p = curr;
             *last_p = curr;
+
         }
+
     }
+
     free(segment);
+
 }
 
 // --- MAIN MPI COORDINATOR ---
@@ -108,11 +160,14 @@ int main(int argc, char *argv[]) {
 
     // 3. Segmented Loop
     for (long curr_low = low_bound; curr_low <= high_bound; curr_low += SEGMENT_SIZE) {
+
         long curr_high = curr_low + SEGMENT_SIZE - 1;
+
         if (curr_high > high_bound) curr_high = high_bound;
 
         sieve_segment(curr_low, curr_high, primes, prime_count,
                       &local_max_gap, &prev_p, &first_p, &last_p);
+
     }
 
     // 4. Consolidate Results
@@ -125,19 +180,27 @@ int main(int argc, char *argv[]) {
     MPI_Reduce(&local_max_gap, &global_max_gap, 1, MPI_LONG_LONG, MPI_MAX, 0, MPI_COMM_WORLD);
 
     if (id == 0) {
+
         // Check gaps between process boundaries
         for (int i = 0; i < p - 1; i++) {
+
             if (all_lasts[i] != -1 && all_firsts[i+1] != -1) {
+
                 long bridge = all_firsts[i+1] - all_lasts[i];
                 if (bridge > global_max_gap) global_max_gap = bridge;
+
             }
+
         }
+
         printf("Result for n = %ld: Largest Gap = %ld\n", n, global_max_gap);
         printf("Time: %f seconds using %d processes\n", MPI_Wtime() - start_time, p);
         free(all_firsts); free(all_lasts);
+
     }
 
     free(primes);
     MPI_Finalize();
     return 0;
+
 }
